67dcf343a16bf5a4cc255dda34659a0caee2c733
ErfanYousefabadi <erfan.yoosefabadi@gmail.com>
2026-07-07 11:20:01 +0330

    initial commit

---
+++ SpaceGame/Game1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
+
+namespace SpaceGame;
+
+public class Game1 : Core
+{
+    public Game1() : base("Space Game", 1280, 720, false)
+    {
+
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        base.LoadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        base.Draw(gameTime);
+    }
+}
