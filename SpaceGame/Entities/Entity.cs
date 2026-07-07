ca5c65ab75e17e6e788d0d2f9a687d782f0046ef
ErfanYousefabadi <erfan.yoosefabadi@gmail.com>
2026-07-07 21:07:48 +0330

    Added animated type for Entity

--- SpaceGame/Entities/Entity.cs
+++ SpaceGame/Entities/Entity.cs
@@ -5,13 +5,17 @@ using MonoGameLibrary.Graphics;
 
 namespace SpaceGame.Entities;
 
+public enum SpriteType { Normal, Animated }
+
 public abstract class Entity
 {
     protected Sprite _sprite;
+    protected AnimatedSprite _animSprite;
     public Vector2 Position { get; set; }
     public int HP { get; set; }
     public int MaxHP { get; set; }
     public bool IsActive { get; set; }
+    public SpriteType SpriteType { get; }
 
     public Entity(Sprite sprite, int maxHP, Vector2 position)
     {
@@ -20,20 +24,41 @@ public abstract class Entity
         HP = maxHP;
         IsActive = HP > 0;
         Position = position;
+        SpriteType = SpriteType.Normal;
+    }
+
+    public Entity(AnimatedSprite sprite, int maxHP, Vector2 position)
+    {
+        _animSprite = sprite;
+        MaxHP = maxHP;
+        HP = maxHP;
+        IsActive = HP > 0;
+        Position = position;
+        SpriteType = SpriteType.Animated;
     }
 
     public virtual void Draw()
     {
-        _sprite.Draw(Core.SpriteBatch, Position);
+        if (SpriteType == SpriteType.Normal)
+            _sprite.Draw(Core.SpriteBatch, Position);
+        else 
+            _animSprite.Draw(Core.SpriteBatch, Position);
     }
 
     public virtual Circle GetBounds()
     {
-        return new Circle(
-            (int)(Position.X + _sprite.Width * 0.5f),
-            (int)(Position.Y + _sprite.Height * 0.5f),
-            (int)(_sprite.Width * 0.5f)
-        );
+        if (SpriteType == SpriteType.Normal)
+            return new Circle(
+                (int)(Position.X + _sprite.Width * 0.5f),
+                (int)(Position.Y + _sprite.Height * 0.5f),
+                (int)Math.Max(_sprite.Width * 0.5f, _sprite.Height * 0.5f)
+            );
+        else    
+            return new Circle(
+                (int)(Position.X + _animSprite.Width * 0.5f),
+                (int)(Position.Y + _animSprite.Height * 0.5f),
+                (int)Math.Max(_animSprite.Width * 0.5f, _animSprite.Height * 0.5f)
+            );
     }
 
     public virtual void TakeDamage(int amount)
