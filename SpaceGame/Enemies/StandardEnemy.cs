fd39816e7806f6f24cde1ee994e70d2c403b9e6f
ErfanYousefabadi <erfan.yoosefabadi@gmail.com>
2026-07-08 16:33:48 +0330

    implemented StandardEnemy

---
+++ SpaceGame/Enemies/StandardEnemy.cs
@@ -0,0 +1,23 @@
+using Microsoft.Xna.Framework;
+using MonoGameLibrary.Graphics;
+
+namespace SpaceGame.Enemies;
+
+public class StandardEnemy : Enemy
+{
+    public StandardEnemy(Sprite sprite, Vector2 pos, float baseSpeed, int baseHP, 
+        int scoreValue, float coinDropChance, Vector2? target) 
+        : base(sprite, pos, baseSpeed, baseHP, scoreValue, coinDropChance, target) {}
+
+    public StandardEnemy(AnimatedSprite sprite, Vector2 pos, float baseSpeed, int baseHP, 
+        int scoreValue, float coinDropChance, Vector2? target) 
+        : base(sprite, pos, baseSpeed, baseHP, scoreValue, coinDropChance, target) {}
+
+    public override void Move(GameTime gameTime)
+    {
+        float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;    
+
+        var newPos = Position;
+        newPos.Y += Speed * deltaTime;
+    }
+}
