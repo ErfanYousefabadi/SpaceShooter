c929c26c6ae3b3c5df0e0f0e94590af9d24eff11
ErfanYousefabadi <erfan.yoosefabadi@gmail.com>
2026-07-10 22:59:52 +0330

    implemented SpriteFactories

---
+++ SpaceGame/Managers/SpriteFactory.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using MonoGameLibrary.Graphics;
+
+namespace SpaceGame.Managers;
+
+public class SpriteFactory
+{
+    private TextureAtlas _atlas;
+    private TextureRegion _shipRegion;
+    private TextureRegion _bulletRegion;
+    private Animation _explosionAnimation;
+
+    private Dictionary<EnemyType, TextureRegion> _enemyRegions;
+
+    public SpriteFactory(TextureAtlas atlas)
+    {
+        _atlas = atlas;
+
+        _shipRegion = _atlas.GetRegion("ship");
+        _bulletRegion = _atlas.GetRegion("bullet");
+        _explosionAnimation = _atlas.GetAnimation("explosion-animation");
+
+        _enemyRegions = new Dictionary<EnemyType, TextureRegion>
+        {
+            { EnemyType.Standard, _atlas.GetRegion("ship") },
+            { EnemyType.Scout, _atlas.GetRegion("ship") },
+            { EnemyType.Shooter, _atlas.GetRegion("ship") },
+            { EnemyType.HeavyTank, _atlas.GetRegion("ship") },
+            { EnemyType.Terrorist, _atlas.GetRegion("ship") },
+        };
+    }
+
+    public Sprite CreateShipSprite() 
+        => CenterOrigin(new Sprite(_shipRegion));
+
+    public Sprite CreateBulletSprite() 
+        => CenterOrigin(new Sprite(_bulletRegion));
+
+    public Sprite CreateEnemySprite(EnemyType type) 
+        => CenterOrigin(new Sprite(_enemyRegions[type]));
+
+    public AnimatedSprite CreateExplosionSprite() 
+        => CenterOrigin(new AnimatedSprite(_explosionAnimation));
+
+    private static Sprite CenterOrigin(Sprite x)
+    {
+        x.CenterOrigin();
+        return x;
+    }
+
+    private static AnimatedSprite CenterOrigin(AnimatedSprite x)
+    {
+        x.CenterOrigin();
+        return x;
+    }
+}
